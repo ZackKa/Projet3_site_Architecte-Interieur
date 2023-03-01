8817c5288c5b49d6fbd0fbb70956ec15a10d8760
ZKA <zaki_le_kids@hotmail.fr>
2023-03-01 11:30:59 +0100

    mise en place des flitres

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function getInfosGallery() {
     fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
     .then((data) => {
-        console.log("tableau: ", data);
+        /**console.log("tableau: ", data);**/
         loadInfosGallery(data);
         projets = data;
     })
@@ -20,8 +20,6 @@ function loadInfosGallery(infos) {
     for (let i = 0; i < infos.length; i++){
         const imageUrl = infos[i].imageUrl;
         const title = infos[i].title;
-        console.log(imageUrl);
-        console.log(title);
 
         const sectionFiches = document.querySelector(".gallery");
         const pieceElement = document.createElement("figure");
@@ -36,6 +34,50 @@ function loadInfosGallery(infos) {
         pieceElement.appendChild(nom);
 
     }
-}
-loadInfosGallery();    
+};
+  
+loadInfosGallery(projets);
 
+const boutonTous = document.querySelector(".btn-tous");
+
+boutonTous.addEventListener("click", function () {
+    const tousFiltrees = projets.filter(function (projet) {
+        return projet;
+    });
+    /**console.log("tous", tousFiltrees);**/
+    document.querySelector(".gallery").innerHTML = "";
+    loadInfosGallery(tousFiltrees);
+});
+
+const boutonObjets = document.querySelector(".btn-objets");
+
+boutonObjets.addEventListener("click", function () {
+    const objetsFiltrees = projets.filter(function (projet) { /*projets.filter(projet => (projet.categoryId == 1));*/
+        return (projet.categoryId == 1);
+    });
+   /**console.log("objet", objetsFiltrees);**/
+   document.querySelector(".gallery").innerHTML = "";
+    loadInfosGallery(objetsFiltrees);
+});
+
+const boutonAppartements = document.querySelector(".btn-appartements");
+
+boutonAppartements.addEventListener("click", function () {
+    const appartementsFiltrees = projets.filter(function (projet) {
+        return (projet.categoryId == 2);
+    });
+   /**console.log("appart", appartementsFiltrees);**/
+   document.querySelector(".gallery").innerHTML = "";
+    loadInfosGallery(appartementsFiltrees);
+});
+
+const boutonHotels = document.querySelector(".btn-hotels");
+
+boutonHotels.addEventListener("click", function () {
+    const hotelsFiltrees = projets.filter(function (projet) {
+        return (projet.categoryId == 3);
+    });
+   /**console.log("hotel", hotelsFiltrees);**/
+   document.querySelector(".gallery").innerHTML = "";
+    loadInfosGallery(hotelsFiltrees);
+});
